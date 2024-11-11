4883c42621dc7d764c871bee1bbc8b7a6351c477
S Gholhaki <17592420+sxg024@users.noreply.github.com>
2024-11-11 19:07:10 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@
 #set -e
 passwd
 read -p "Please enter your SSH public key: " ssh_key_pair
-cat ssh_key_pair >> ~/.ssh/authorized_keys
+echo ssh_key_pair >> ~/.ssh/authorized_keys
 sed -i '/^PermitRootLogin[ \t]\+\w\+$/{ s//PermitRootLogin no/g; }' /etc/ssh/sshd_config
+sudo service ssh restart
